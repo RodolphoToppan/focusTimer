cf7e00ebf8a180a518b89b3450370d31d8625ef1
Rodolpho Toppan <105832266+RodolphoToppan@users.noreply.github.com>
2023-02-27 13:08:26 -0300

    Add files via upload

---
+++ js/elements.js
@@ -0,0 +1,25 @@
+const cards = document.getElementsByClassName('card')
+const cardTree = document.querySelector('#tree')
+const cardRain = document.querySelector('#rain')
+const cardHouse = document.querySelector('#house')
+const cardFire = document.querySelector('#fire')
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonPlus = document.querySelector('.plus')
+const buttonMinus = document.querySelector('.minus')
+const secondsDisplay = document.querySelector('.seconds')
+const minutesDisplay = document.querySelector('.minutes')
+
+export {
+  cards,
+  cardTree,
+  cardRain,
+  cardHouse,
+  cardFire,
+  buttonPlay,
+  buttonStop,
+  buttonPlus,
+  buttonMinus,
+  secondsDisplay,
+  minutesDisplay
+}
